b3b209a305278a8e98fa3fc46924ffcbf80ef869
skanehira <sho19921005@gmail.com>
2021-10-16 03:25:21 +0900

    Part 1 - Introduction and Setting up the REPL

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
 
+	"github.com/containerd/console"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+func main() {
+	current := console.Current()
+	if err := current.DisableEcho(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer func() {
+		_ = current.Reset()
+	}()
+
+	if err := current.SetRaw(); err != nil {
+		log.Println(err)
+		return
+	}
+
+	term := terminal.NewTerminal(current, "db > ")
+
+	for {
+		line, err := term.ReadLine()
+		if err != nil {
+			break
+		}
+
+		if strings.HasSuffix(line, ".exit") {
+			break
+		}
+		fmt.Printf("Unrecognized command '%s'.\n", line)
+	}
 }
